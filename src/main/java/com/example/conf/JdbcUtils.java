0313de57943b765452bbb018f23c131bc70b6df1
chung nguyen <chungadadad@gmail.com>
2023-07-02 13:25:35 +0700

    Thay database mysql bang sqlite

--- src/main/java/com/example/conf/JdbcUtils.java
+++ src/main/java/com/example/conf/JdbcUtils.java
@@ -5,18 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcUtils {
-    private static Connection conn;
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
     }
 
     public static Connection getConn() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/ooopappdb",
-                "root", "chung224864@");
+        return DriverManager.getConnection("jdbc:sqlite:src/main/java/db/oopapp.db");
     }
 }
